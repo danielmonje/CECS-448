493267810af260c78a3c66a033643d8d8230c809
MichaelZ1853 <MichaelZ1853@gmail.com>
2021-02-16 10:29:23 -0800

    Added comments for progress bar

--- Assignment1.js
+++ Assignment1.js
@@ -8,23 +8,27 @@ function showPreview(event) {
 }
 
 $(document).ready(function(){
+    //shows upload form screen
     $('#userPic a').click(function(){
         $('#upload-form').show();
     });
 
+    //"uploads" an image for 2 seconds before closing the upload form screen
     $('#upload-form a').click(function(){
         $('#bar').show();
         $('#progress-bar').show();
+        //FIXME this is used to update width of the blue progression bar (bugged out)
         var progress = document.getElementById("progress-bar");
         var newWidth = progress.style.width;
         setTimeout(function(){ 
-            //newWidth = "50%";
+            newWidth = "50%";
             $('#bar').text("50%")
-        }, 1000);
+        }, 1000); //updates to 50% after 1 second
         setTimeout(function(){ 
             $('#bar').text("100%")
-            // newWidth = "100%";
-        }, 2000);
+            newWidth = "100%";
+        }, 2000); //updates to 100% after 2 seconds
+        //Hides the upload form menu after 2 seconds
         // $('#upload-form').hide();
         // $('#bar').hide();
         // $('#progress-bar').hide();
