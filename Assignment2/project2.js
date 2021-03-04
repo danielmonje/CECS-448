4f7cec30407456050372c9fcc34d3e6fcc777829
jigom11791 <jigom11791@gmail.com>
2021-03-04 13:22:58 -0800

    added animations to tabs

--- Assignment2/project2.js
+++ Assignment2/project2.js
@@ -1,8 +1,10 @@
 const panels = document.querySelectorAll('.panel');
 const tabLink = document.querySelectorAll('.tab-link');
+const tabLinkText = document.querySelectorAll('.tab-link span');
 const tabPanes = document.querySelectorAll('.tab-pane');
 const paneImage = document.querySelectorAll('.pane-image');
 const paneDesc = document.querySelectorAll('.pane-description');
+active = 0;
 //removeActiveClasses();
 
 panels.forEach((panel) => {
@@ -15,11 +17,35 @@ panels.forEach((panel) => {
 tabLink.forEach((panel, index) => {
     panel.addEventListener('click', () => {
         removeActiveClassesTabs();
+        active = index;
         panel.classList.add('active');
         tabPanes[index].classList.add('active');
         tabPanes[index].classList.add('d-flex');
-        
-
+        anime({
+            targets: [paneImage[index], paneDesc[index]],
+            opacity: 0,
+            duration: 0,
+            translateX: -20,
+            translateY: -20,
+            loop: false
+        });
+        anime({
+                targets: paneImage[index],
+                opacity: 1,
+                duration: 2000,
+                translateX: 0,
+                translateY: 0,
+                loop: false
+        });
+        anime({
+            targets: paneDesc[index],
+            delay: 400,
+            opacity: 1,
+            duration: 2000,
+            translateX: 0,
+            translateY: 0,
+            loop: false
+    });
     });
 });
 
